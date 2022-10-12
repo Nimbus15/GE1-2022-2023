6b6cfafffb8595d710b662d191c795cace1accad
Alex Bang <Alex.ZBP2456@GMAIL.COM>
2022-10-12 18:09:30 +0100

    started with variable

---
+++ GE1 Examples 2022/Assets/FishScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishScript : MonoBehaviour
+{
+    public Transform head;
+    public Transform tail;
+
+    [Range(0, 1.0f)]
+    public float frequency;
+    public int headAmplitude;
+    public int tailAmplitude;
+    public float theta;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+               
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
