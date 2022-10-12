15be44cc376082af4e618cf9a984cf96969c0205
Alex Bang <Alex.ZBP2456@GMAIL.COM>
2022-10-12 19:31:04 +0100

    trying to get it to work

--- GE1 Examples 2022/Assets/FishScript.cs
+++ GE1 Examples 2022/Assets/FishScript.cs
@@ -16,13 +16,14 @@ public class FishScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-               
-
-    }
+        head = gameObject.transform.Find("headCube");   
+        tail = gameObject.transform.Find("tailCube");  
+    } 
 
     // Update is called once per frame
     void Update()
     {
-        
+        head.transform.rotation = new Transform(head.transform.rotation.x, head.transform.rotation.y, head.transform.rotation.y + Mathf.Sin(theta) * headAmplitude);
+        tail.transform.rotation = new Transform(tail.transform.rotation.x, tail.transform.rotation.y, tail.transform.rotation.y + Mathf.Sin(theta) * tailAmplitude);
     }
 }
