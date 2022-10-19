e6aa7724e2a60faa3f9481c61b0b0814bed228ed
Nimbus15 <Alex.ZBP2456@GMAIL.COM>
2022-10-19 20:04:57 +0100

    attempt 2

--- GE1 Examples 2022/Assets/MyEnemy.cs
+++ GE1 Examples 2022/Assets/MyEnemy.cs
@@ -2,10 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MyAgent : MonoBehaviour
+public class MyEnemy : MonoBehaviour
 {
     public GameObject plain;
     public GameObject[] waypoints;
+    private int currentWaypoint = 0;
+    private float speed = 1.0f;
+    private float rotationSpeed = 1.0f;
+
     // Start is called before the first frame update
     void Start()
     {
