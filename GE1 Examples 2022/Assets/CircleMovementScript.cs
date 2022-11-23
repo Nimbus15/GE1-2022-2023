e2796fb737a46bab9e764adb169c403ec7b60016
Nimbus15 <Alex.ZBP2456@GMAIL.COM>
2022-11-23 18:56:01 +0000

    start lab8

---
+++ GE1 Examples 2022/Assets/CircleMovementScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleMovementScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public List<Vector3> path;
+
+    void Start()
+    {
+        
+    }
+}
