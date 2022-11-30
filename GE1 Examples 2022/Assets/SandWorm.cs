b3f9f35739b361d8783f181d6b761b53a979db5a
Nimbus15 <Alex.ZBP2456@GMAIL.COM>
2022-11-30 21:57:36 +0000

    Wriggle script

---
+++ GE1 Examples 2022/Assets/SandWorm.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SandWorm : MonoBehaviour
+{
+    public Vector3 startBody;
+    public Vector3 middleBody;
+    public Vector3 endBody;
+
+    public float speedOfWriggle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator wrigglingCo()
+    {
+        yield return null;
+    }
+}
